16a4c35ca2935df9b5b39e35beeeeab8c58f672f
kwan3217 <kwan3217@gmail.com>
2024-06-16 14:40:04 -0600

    Add code to better debug the canse when there is no interpreter for a compressed instruction.

--- src/riscv/hart.py
+++ src/riscv/hart.py
@@ -346,8 +346,12 @@ class Hart:
             except WrongInterpreter:
                 continue
         if not handled:
-            from riscv.rv32i import I
-            raise ValueError(f"At pc=0x{self.pc:08x}, unhandled instruction {I(ins, self.XLEN, True)}")
+            if read_bitfield(ins,1,0)==0b11:
+                from riscv.rv32i import I
+                raise ValueError(f"At pc=0x{self.pc:08x}, unhandled instruction {I(ins, self.XLEN, True)}")
+            else:
+                raise ValueError(f"At pc=0x{self.pc:08x}, unhandled compressed instruction "
+                                 f"0b{read_bitfield(ins,15,13):03b}_{read_bitfield(ins,12,12):01b}_{read_bitfield(ins,11,7):05b}_{read_bitfield(ins,6,2):05b}_{read_bitfield(ins,1,0):02b}")
         else:
             if not self._pc_changed:
                 self.pc += length
