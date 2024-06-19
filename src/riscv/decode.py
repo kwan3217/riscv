4c12a972af0ba47c13cde52c18f2d29867515e0b
kwan3217 <kwan3217@gmail.com>
2024-06-19 16:17:22 -0600

    Add more encodings

--- src/riscv/decode.py
+++ src/riscv/decode.py
@@ -179,7 +179,31 @@ C32I_encodings={
     "|__ | || ... || ... _|":None, # Reserved
     "|_| B498A673215 _|":"C.J",
     "||_ 843 mmm 76215 _|":Handler(name="C.BEQZ"),
-    "||| 843 mmm 76215 _|":Handler(name="C.BNEZ")
+    "||| 843 mmm 76215 _|":Handler(name="C.BNEZ"),
+    # Quadrant 2
+    "___ _ fffff 43210 |_":Handler("C.SLLI",sign='+'), # Bit 12 is nzimm5, which must be 0 for C32I
+    "___ 5 fffff 43210 |_":None, # C.SLLI on C64/128I, reserved for non-standard extensions on C32I
+    "___ _ fffff _____ |_":Handler("C.HINT7",sign='+'), # C.SLLI rd,64 on C128I, hint on C32/64I
+    "__| 5 ddddd 43876 |_":None, # C.FLDSP, C32/64F
+    "__| 5 ddddd 49876 |_":None, # C.LQSP, C128I
+    "_|_ 5 ddddd 43276 |_":Handler("C.LWSP",sign='+'),
+    "_|_ 5 _____ 43276 |_":None, # Reserved, equivalent to C.LWSP x0,imm
+    "_|| 5 ddddd 43276 |_":None, # C.FLWSP
+    "_|| 5 ddddd 43876 |_":None, # C.LDSP, C64/128I
+    "_|| 5 _____ 43876 |_":None, # Reserved, equivalent to C.LDSP x0,imm
+    "|__ _ lllll _____ |_":Handler("C.JR"),  # Encoding that C.MV rd=x0 *would* have
+    "|__ _ _____ _____ |_":None, # Reserved, equivalent to C.JR x0
+    "|__ _ ddddd zzzzz |_":Handler("C.MV"),
+    "|__ _ _____ zzzzz |_":Handler("C.HINT8"), # Equivalent to C.MV x0=rs2
+    "|__ | _____ _____ |_":Handler("C.EBREAK"), # Encoding that C.ADD x0,x0 *would* have
+    "|__ | lllll _____ |_":Handler("C.JALR"), # Encoding that C.ADD rs1/rd,x0 *would* have
+    "|__ | fffff zzzzz |_":Handler("C.ADD"),
+    "|__ | _____ zzzzz |_":Handler("C.HINT9"), # Encoding that C.ADD x0,rs2 *would* have
+    "|_| 543876 zzzzz |_":None, # C.FSDSP, CF
+    "|_| 549876 zzzzz |_":None, # C.SQSP, C128I
+    "||_ 543276 zzzzz |_":Handler("C.SWSP"),
+    "||| 543276 zzzzz |_":None, # C.FSWSP, C32F
+    "||| 543876 zzzzz |_":None, # C.SDSP, C64/128F
 }
 
 C_encodings={}
