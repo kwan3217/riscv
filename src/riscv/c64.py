1f56fd278c6c9178cf2a8caef806f5333bc03475
kwan3217 <kwan3217@gmail.com>
2024-06-24 11:26:04 -0600

    Use more instruction handlers from I for C. Some of the C handlers were better (I wrote them later) so merge the best features back into I.

--- src/riscv/c64.py
+++ src/riscv/c64.py
@@ -19,11 +19,11 @@ class C64(InstructionSet):
         "+_|| 543 mmm 76 eee __": I.Load('C.LD',size=8,signed=False),
         "+||| 543 mmm 76 yyy __": I.Store('C.SD',size=8),
         "-__| 5 fffff 43210  _|":I.RegImmed("C.ADDIW", "i32(%s+%s)", lambda hart, rs1, imm: rs1 + imm,w=32),
-        "+|__ 5 __ ggg 43210 _|": C.C_RegImmed("C.SRLI", ">L>", lambda hart, rs1, imm: rs1 >> imm),  # Bit 12 is nzimm5, which must be 0 for RV32C
-        "+|__ 5 _| ggg 43210 _|": C.C_RegImmed("C.SRAI", ">A>", lambda hart, rs1, imm: hart.signed(rs1) >> imm),  # Bit 12 is nzimm5, which must be 0 for RV32C
+        "+|__ 5 __ ggg 43210 _|": I.RegImmed("C.SRLI", ">L>", lambda hart, rs1, imm: rs1 >> imm),  # Bit 12 is nzimm5, which must be 0 for RV32C
+        "+|__ 5 _| ggg 43210 _|": I.RegImmed("C.SRAI", ">A>", lambda hart, rs1, imm: hart.signed(rs1) >> imm),  # Bit 12 is nzimm5, which must be 0 for RV32C
         " |__ | || ggg __ yyy _|":I.RegReg("C.SUBW", "i32(%s-%s)", lambda hart, rs1, rs2: rs1 - rs2,w=32),
         " |__ | || ggg _| yyy _|":I.RegReg("C.ADDW", "i32(%s+%s)", lambda hart, rs1, rs2: rs1 + rs2,w=32),
-        "+___ 5 fffff 43210 |_": C.C_RegImmed("C.SLLI", "<<", lambda hart, rs1, imm: rs1 << imm),  # Bit 12 is nzimm5, which must be 0 for C32I
+        "+___ 5 fffff 43210 |_": I.RegImmed("C.SLLI", "<<", lambda hart, rs1, imm: rs1 << imm),  # Bit 12 is nzimm5, which must be 0 for C32I
         "+_|| 5 ddddd 43876 |_": C.C_LoadSP("C.LDSP",size=8),
         "+||| 543876 zzzzz |_": C.C_StoreSP("C.SDSP",size=8),
 
